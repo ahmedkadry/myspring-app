53537dbe5ce172b9206f878f37560e2ed7c11ddb
ahmedkadry <ahmedkadry4work@gmail.com>
2022-08-12 17:22:25 +0200

    The first spring boot app commit

--- src/main/java/com/A7meD/springboot/controller/HomeController.java
+++ src/main/java/com/A7meD/springboot/controller/HomeController.java
@@ -7,13 +7,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class HomeController {
 
-	@GetMapping("/")
+	@GetMapping("/test")
 	@ResponseBody
 	public String test() {
 		return "ALLAH AKBER";
 	}
 
-	@GetMapping("/home")
+	@GetMapping("/")
 	public String home() {
 		return "home";
 	}
